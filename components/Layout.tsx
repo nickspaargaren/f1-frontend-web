881a7688a2bb831ef8141d785c3ef7491d70969a
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-17 18:37:19 +0100

    Header component capital

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 
-import Header from '@/components/Header';
+import Header from '@/components/header';
 
 interface LayoutProps {
   children: any;
