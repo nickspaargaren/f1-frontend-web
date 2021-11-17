2dfea6f1135909ba051eaee5f968aab43c3c13c6
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-17 22:22:01 +0100

    Import git fixes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 
-import Header from '@/components/Headerr';
+import Header from '@/components/Header';
 
 interface LayoutProps {
   children: any;
--- hooks/useCircuits.tsx
+++ hooks/useCircuits.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Circuit } from 'types';
+
+import { Circuit } from '../types';
 
 const useCircuits = () => {
   const [circuits, setCircuits] = useState<{
