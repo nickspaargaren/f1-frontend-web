7cbb3205741b6abf145ef438751d43f699dffaba
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-11-24 21:02:23 +0100

    Add loading animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,6 +12,19 @@ import useCircuits from '@/hooks/useCircuits';
 const Home: NextPage = (): ReactElement => {
   const circuits = useCircuits('https://f1-api.vercel.app/api/circuits');
 
+  const containerAnimation = {
+    show: {
+      transition: {
+        staggerChildren: 0.05,
+      },
+    },
+  };
+
+  const itemAnimation = {
+    hidden: { opacity: 0, transform: 'translateY(5px)' },
+    show: { opacity: 1, transform: 'translateY(0px)' },
+  };
+
   if (circuits.error) {
     return <Layout title="F1 stats" description="Circuits">{circuits.error}</Layout>;
   }
@@ -21,30 +35,38 @@ const Home: NextPage = (): ReactElement => {
 
   return (
     <Layout title="F1 stats" description="Circuits">
-      {circuits.data.circuits
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map((item) => (
-          <div key={item._id} className="circuit">
-            <Link href={`/circuits/${item.name}`}>
-              <a>
-                <div className="image">
-                  {item.flag
+      <motion.div
+        variants={containerAnimation}
+        initial="hidden"
+        animate="show"
+      >
+        {circuits.data.circuits
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((item) => (
+            <motion.div variants={itemAnimation}>
+              <div key={item._id} className="circuit">
+                <Link href={`/circuits/${item.name}`}>
+                  <a>
+                    <div className="image">
+                      {item.flag
                       && <Image src={`/images/flags/${item.flag}.png`} alt={item.flag} width={30} height={18} />}
-                </div>
-                <div className="title">
-                  {item.name}
-                  <br />
-                  <small>{item.description}</small>
-                </div>
-                <div className="winner">
-                  {item.winner && (
-                    <Winner title={item.winner} />
-                  )}
-                </div>
-              </a>
-            </Link>
-          </div>
-        ))}
+                    </div>
+                    <div className="title">
+                      {item.name}
+                      <br />
+                      <small>{item.description}</small>
+                    </div>
+                    <div className="winner">
+                      {item.winner && (
+                      <Winner title={item.winner} />
+                      )}
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            </motion.div>
+          ))}
+      </motion.div>
     </Layout>
   );
 };
