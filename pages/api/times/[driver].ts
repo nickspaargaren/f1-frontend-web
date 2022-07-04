5b41882e8a6c87f83270e743ffa7f22b529bfc72
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-07-04 21:28:16 +0200

    Update api status codes & early returns

--- pages/api/times/[driver].ts
+++ pages/api/times/[driver].ts
@@ -9,103 +9,103 @@ export default async function handler(
 ) {
   const { apikey, driver, time, circuit } = req.query;
 
-  if (apikey === process.env.API_KEY) {
-    switch (req.method) {
-      case "GET":
-        try {
-          const times = await prisma.times.findMany({
-            where: {
-              gamertag: driver as string,
-            },
-          });
+  if (apikey !== process.env.API_KEY) {
+    res.status(401).json({ success: false, data: "Invalid API key" });
+  }
 
-          if (times.length) {
-            res.status(200).json({ success: true, data: { times } });
-          } else {
-            res.status(200).json({
-              success: false,
-              data: { times: `No times set for user ${driver}` },
-            });
-          }
-        } catch (error) {
-          res.status(400).json({ success: false });
+  switch (req.method) {
+    case "GET":
+      try {
+        const times = await prisma.times.findMany({
+          where: {
+            gamertag: driver as string,
+          },
+        });
+
+        if (times.length) {
+          res.status(200).json({ success: true, data: { times } });
+        } else {
+          res.status(200).json({
+            success: false,
+            data: { times: `No times set for user ${driver}` },
+          });
         }
-        break;
-      case "POST":
-        try {
-          const newtime = await prisma.times.upsert({
-            where: {
-              gamertag_circuit: {
-                gamertag: driver as string,
-                circuit: circuit as string,
-              },
-            },
-            update: {
-              time: time as string,
-              gamertag: driver as string,
-              circuit: circuit as string,
-            },
-            create: {
-              time: time as string,
-              gamertag: driver as string,
-              circuit: circuit as string,
-            },
-          });
-
-          const times = await prisma.times.findMany({
-            where: {
-              circuit: circuit as string,
-            },
-            take: 1,
-            orderBy: {
-              time: "asc",
-            },
-          });
-
-          let newWinner;
-          if (times) {
-            newWinner = await prisma.circuits.update({
-              where: {
-                name: circuit as string,
-              },
-              data: {
-                winner: times[0].gamertag,
-              },
-            });
-          }
-
-          res.status(201).json({ success: true, data: newtime, newWinner });
-        } catch (error) {
-          res.status(400).json({ success: false });
-        }
-        break;
-      case "DELETE":
-        try {
-          const deleteTime = await prisma.times.deleteMany({
-            where: {
-              gamertag: driver as string,
-              circuit: circuit as string,
-            },
-          });
-
-          if (deleteTime.count === 0) {
-            res
-              .status(201)
-              .json({ success: false, message: "Time does not exist" });
-          } else {
-            res
-              .status(201)
-              .json({ success: true, message: `Deleted ${driver}'s time ` });
-          }
-        } catch (error) {
-          res.status(400).json({ success: false });
-        }
-        break;
-      default:
+      } catch (error) {
         res.status(400).json({ success: false });
-        break;
-    }
-  } else {
-    res.status(400).json({ success: false, data: "Invalid API key" });
+      }
+      break;
+    case "POST":
+      try {
+        const newtime = await prisma.times.upsert({
+          where: {
+            gamertag_circuit: {
+              gamertag: driver as string,
+              circuit: circuit as string,
+            },
+          },
+          update: {
+            time: time as string,
+            gamertag: driver as string,
+            circuit: circuit as string,
+          },
+          create: {
+            time: time as string,
+            gamertag: driver as string,
+            circuit: circuit as string,
+          },
+        });
+
+        const times = await prisma.times.findMany({
+          where: {
+            circuit: circuit as string,
+          },
+          take: 1,
+          orderBy: {
+            time: "asc",
+          },
+        });
+
+        let newWinner;
+        if (times) {
+          newWinner = await prisma.circuits.update({
+            where: {
+              name: circuit as string,
+            },
+            data: {
+              winner: times[0].gamertag,
+            },
+          });
+        }
+
+        res.status(201).json({ success: true, data: newtime, newWinner });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "DELETE":
+      try {
+        const deleteTime = await prisma.times.deleteMany({
+          where: {
+            gamertag: driver as string,
+            circuit: circuit as string,
+          },
+        });
+
+        if (deleteTime.count === 0) {
+          res
+            .status(201)
+            .json({ success: false, message: "Time does not exist" });
+        } else {
+          res
+            .status(201)
+            .json({ success: true, message: `Deleted ${driver}'s time ` });
+        }
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
   }
 }
--- pages/api/times/index.ts
+++ pages/api/times/index.ts
@@ -7,22 +7,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.apikey === process.env.API_KEY) {
-    switch (req.method) {
-      case "GET":
-        try {
-          const times = await prisma.times.findMany();
+  if (req.query.apikey !== process.env.API_KEY) {
+    res.status(401).json({ success: false, data: "Invalid API key" });
+  }
 
-          res.status(200).json({ success: true, data: { times } });
-        } catch (error) {
-          res.status(400).json({ success: false });
-        }
-        break;
-      default:
+  switch (req.method) {
+    case "GET":
+      try {
+        const times = await prisma.times.findMany();
+
+        res.status(200).json({ success: true, data: { times } });
+      } catch (error) {
         res.status(400).json({ success: false });
-        break;
-    }
-  } else {
-    res.status(400).json({ success: false, data: "Invalid API key" });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
   }
 }
