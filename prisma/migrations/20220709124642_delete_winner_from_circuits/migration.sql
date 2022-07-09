f0307aa236db7f33625d25bda25ab3732519f3f9
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-07-09 14:48:29 +0200

    Delete winner from circuits

---
+++ prisma/migrations/20220709124642_delete_winner_from_circuits/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `winner` on the `Circuits` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Circuits" DROP COLUMN "winner";
