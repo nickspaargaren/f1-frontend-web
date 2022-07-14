65522b5f943f51a8c531e478eb92b90ec85b4606
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-07-14 21:05:17 +0200

    fix migrations

--- prisma/migrations/20220709124642_delete_winner_from_circuits/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `winner` on the `Circuits` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Circuits" DROP COLUMN "winner";
--- prisma/migrations/20220714184914_added_autoincrement_to_circuit_id/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE "times_circuitid_seq";
-ALTER TABLE "Times" ALTER COLUMN "circuitId" SET DEFAULT nextval('times_circuitid_seq');
-ALTER SEQUENCE "times_circuitid_seq" OWNED BY "Times"."circuitId";
