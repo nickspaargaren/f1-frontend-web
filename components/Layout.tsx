153bde8f29c949424a50c731470c9ccf8d1104e1
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-09-18 15:29:36 +0200

    Add Plausible Analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,6 +20,7 @@ const Layout = ({
       <title>{title} | Racetijden</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+      <script defer data-domain="racetijden.nl" src="/js/script.js"></script>
     </Head>
 
     <Header title={title} winner={winner} />
--- next.config.js
+++ next.config.js
@@ -25,4 +25,16 @@ module.exports = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/js/script.js",
+        destination: "https://plausible.io/js/script.js",
+      },
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 };
