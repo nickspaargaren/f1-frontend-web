197bc3c4cbf432a048b5fbe3a914b2a8eda6e375
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2023-08-13 18:34:20 +0200

    Update loading component with translations

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import styled from "styled-components";
 
+import { useTranslation } from "@/helpers/useTranslation";
+
 const StyledLoading = styled.div`
   margin: 0 auto;
   width: 134px;
@@ -12,36 +14,40 @@ const StyledLoading = styled.div`
   font-size: 13px;
 `;
 
-const Loading = (): ReactElement => (
-  <StyledLoading>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="58px"
-      height="58px"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="xMidYMid"
-    >
-      <circle
-        cx="50"
-        cy="50"
-        fill="none"
-        stroke="#e20600"
-        strokeWidth="10"
-        r="35"
-        strokeDasharray="164.93361431346415 56.97787143782138"
+const Loading = (): ReactElement => {
+  const { t } = useTranslation();
+
+  return (
+    <StyledLoading>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="58px"
+        height="58px"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
       >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          repeatCount="indefinite"
-          dur="1s"
-          values="0 50 50;360 50 50"
-          keyTimes="0;1"
-        />
-      </circle>
-    </svg>
-    <div>Laden...</div>
-  </StyledLoading>
-);
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="#e20600"
+          strokeWidth="10"
+          r="35"
+          strokeDasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          />
+        </circle>
+      </svg>
+      <div>{t("loading")}</div>
+    </StyledLoading>
+  );
+};
 
 export default Loading;
