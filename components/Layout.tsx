1d0a8f9914c09b72ed8750e1a162602192d54107
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-05-05 09:56:58 +0200

    Fix Analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
 import React, { PropsWithChildren, ReactElement } from "react";
 
 import Header from "@/components/Header";
@@ -21,7 +20,7 @@ const Layout = ({
       <title>{`${title} | Racetijden`}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
-      <Script defer data-domain="racetijden.nl" src="/js/script.js" />
+      <script defer data-domain="racetijden.nl" src="/js/script.js"></script>
     </Head>
 
     <Header title={title} winner={winner} />
