652a9cb97a472a0289f3f27237dac3b427487ff2
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-11-19 21:24:10 +0100

    Migrate Next config to TypeScript

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   distDir: "build",
   compiler: {
@@ -45,3 +46,5 @@ module.exports = {
     ];
   },
 };
+
+export default nextConfig;
